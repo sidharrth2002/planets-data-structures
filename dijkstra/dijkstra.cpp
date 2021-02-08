efeb9d90de21aba221de7ef699bf1248e7488794
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-02-08 17:20:54 +0800

    Planet and Edge class done(in dijkstra for now)

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
+class Planet {
+    public:
+        string name;
+        int id;
+        int x;
+        int y;
+        int z;
+        int weight;
+        int profit;
+        Planet(int id, string name, int x, int y, int z, int weight, int profit);
+};
+
+Planet::Planet(int id, string name, int x, int y, int z, int weight, int profit) {
+    this->id = id;
+    this->name = name;
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->weight = weight;
+    this->profit = profit;
+}
+
+class Edge {
+    public:
+        Planet* start;
+        Planet* end;
+        int distance;
+        Edge(Planet* start, Planet* end);
+};
+
+Edge::Edge(Planet* start, Planet* end) {
+    this->start = start;
+    this->end = end;
+    //calculate distance between planets
+    this->distance = sqrt(pow(end->x - start->x, 2) + pow(end->y - start->y, 2) + pow(end->z - start->z, 2));
+}
+
+vector<Planet> planets;
+vector<Edge> edges;
+
+void loadFromFile() {
+    ifstream input;
+    input.open("../A2planets.txt");
+
+    string name;
+    int x, y, z, weight, profit;
+
+    int i = 0;
+    //create planets
+    while(input >> name >> x >> y >> z >> weight >> profit) {
+        Planet planet(i++, name, x, y, z, weight, profit);
+        planets.push_back(planet);
+    }
+   input.close();
+
+    //assuming planet A is in the first index, B second and so on
+    //create the edges based on the map
+    input.open("../routes.txt");
+    char startPlanet, endPlanet;
+    int start, end;
+    while(input >> startPlanet >> endPlanet) {
+        //convert planet char to integer
+        start = (int)startPlanet - 65;
+        end = (int)endPlanet - 65;
+        Edge edge(&planets[start], &planets[end]);
+        edges.push_back(edge);
+    }
+    input.close();
+}
+
+//dijkstra starts from here
 const int THEORETICAL_MAX = 1000000;
 const int MAX_ENTRIES = 100;
 int distanceMatrix[MAX_ENTRIES][MAX_ENTRIES];
@@ -10,6 +84,10 @@ int numberOfNodes;
 int passesThrough[MAX_ENTRIES];
 
 void initializeMatrices() {
+    for(int i = 0; i < edges.size(); i++) {
+        distanceMatrix[edges[i].start->id][edges[i].end->id] = edges[i].distance;
+    }
+
     for(int i = 0; i < MAX_ENTRIES; i++) {
         distances[i] = THEORETICAL_MAX;
     }
@@ -28,6 +106,7 @@ int getNearestAdjacent() {
             minimumNodeIndex = i;
         }
     }
+    return minimumNodeIndex;
 }
 
 void displayShortestDistances() {
@@ -43,23 +122,23 @@ void dijkstra() {
         nodesVisited[nearestIndex] = true;
         for(int adjacent = 0; adjacent < numberOfNodes; adjacent++) {
             if(
+                //there is a connection between the 2
                 (distanceMatrix[nearestIndex][adjacent] != THEORETICAL_MAX)
                 &&
-                (distances[adjacent] > distances[nearestIndex] + distanceMatrix[nearestIndex][adjacent]) 
+                //if going through this vertice would help reach destination faster
+                (distances[nearestIndex] + distanceMatrix[nearestIndex][adjacent] < distances[adjacent]) 
             ) {
+                //this is now the new distance
                 distances[adjacent] = distances[nearestIndex] + distanceMatrix[nearestIndex][adjacent];
+                //to get to this node have to pass through this index
+                //can trace path later
                 passesThrough[adjacent] = nearestIndex;
             }
         }
     }
-    displayShortestDistances();
-}
-
-void loadFromFile() {
-
 }
 
 int main() {
     loadFromFile();
-    cout << "at least runs" << endl;
+    initializeMatrices();
 }
